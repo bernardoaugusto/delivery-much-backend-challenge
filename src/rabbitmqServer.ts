b1250d7b09c88e0aebe786a3b8dc778b8c0361c1
Bernardo Augusto Bastos Silva <bernardo.augusto.bastos@gmail.com>
2021-01-29 18:10:24 -0300

    feat: add RabbitMQ

---
+++ src/rabbitmqServer.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { Connection, Channel, connect, Message } from 'amqplib';
+
+export default class RabbitmqServer {
+    private conn: Connection;
+
+    private channel: Channel;
+
+    constructor(private uri: string) {}
+
+    async start(): Promise<void> {
+        this.conn = await connect(this.uri);
+        this.channel = await this.conn.createChannel();
+    }
+
+    async consume(queue: string, callback: (message: Message) => void) {
+        return this.channel.consume(queue, message => {
+            callback(<any>message);
+            this.channel.ack(<any>message);
+        });
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import app from './app';
 
 import connect from './database/connection/connection';
 import './containers';
+import RabbitmqServer from './rabbitmqServer';
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,6 +14,15 @@ async function startServer() {
         // eslint-disable-next-line no-console
         console.log(`Service running on port ${PORT}`);
     });
+
+    const server = new RabbitmqServer('amqp://guest:guest@localhost:5672');
+    await server.start();
+    await server.consume('incremented', message =>
+        console.log(`incremented ${message.content.toString()}`),
+    );
+    await server.consume('decremented', message =>
+        console.log(`decremented ${message.content.toString()}`),
+    );
 }
 
 startServer();
